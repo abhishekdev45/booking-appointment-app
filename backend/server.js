e40a3742eede23933fc6a9f6afc1ae53c8cd3304
abhishekdev45 <abhishek14kl@gmail.com>
2023-12-14 20:34:31 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('mongodb connected')
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Node App running ")
+    })
+}).catch((err) => {
+    console.log(err)
+})
