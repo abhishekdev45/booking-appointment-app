f3d6c51994832366ded0633ae0a26344a3c8ef6d
Sneha Khandelwal <139537271+geeksnow12@users.noreply.github.com>
2023-12-14 23:20:49 +0530

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,8 @@
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import {Routes} from "react-router-dom"
-import {Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-      <Route index element={
-        <IndexPage />
-      } />
-      <Route path="/login" element={
-        <LoginPage/>
-      }/>
-      <Route path="/register" element={
-        <RegisterPage/>
-      }/>
-      </Route>
-      </Routes>
+    <div>Hello</div>
   );
 }
 
