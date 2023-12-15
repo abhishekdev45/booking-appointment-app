d418028e4154965b57bb1072ff8a7c55f8c2c65a
Vishwam Pandey <vishwampandey786@gmail.com>
2023-12-15 13:06:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+<<<<<<< Updated upstream
 import { Adminlogin } from './admin/pages/Adminlogin';
 
 function App() {
@@ -7,7 +8,18 @@ function App() {
     <div className="App">
       
     </div>
+=======
+import CreateDepartment from './admin/pages/CreateDepartment';
+
+function App() {
+  return (
+    <>
+    
+      <CreateDepartment />
+    
+    </>
+>>>>>>> Stashed changes
   );
-}
+};
 
 export default App;
