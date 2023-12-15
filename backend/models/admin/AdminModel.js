44592f386a4d81e5523b4046c90cbfb0fbe0da42
kartikey <kartikey090803@gmail.com>
2023-12-15 12:38:04 +0530

    jj

--- backend/models/admin/AdminModel.js
+++ backend/models/admin/AdminModel.js
@@ -1,1 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+const admSchema = mongoose.Schema(
+    {
+        name:{type:String,require},
+        email:{type:String,require},
+        password:{type:String,require:true},
+        department:{type:String,require:true},
+        role:{type:String,require:true}
+        
+    }
+)
