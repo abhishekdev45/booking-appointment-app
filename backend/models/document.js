fd4ce955cca2884dd600a8e77acd5d56df98283e
abhishekdev45 <abhishek14kl@gmail.com>
2023-12-15 12:56:03 +0530

    hg

---
+++ backend/models/document.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  paths: [
+    {
+      type: String,
+      required: true
+    }
+  ]
+});
+
+const Document = mongoose.model('Document', documentSchema);
+
+module.exports = Document;
